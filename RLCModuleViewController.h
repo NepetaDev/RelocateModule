61b1b378b76714d4377ff59ec3af980c2147f605
Nepeta <ominousness@protonmail.com>
2019-05-14 18:22:31 +0200

    reduce load

--- RLCModuleViewController.h
+++ RLCModuleViewController.h
@@ -1,6 +1,9 @@
 #import <ControlCenterUIKit/CCUIMenuModuleViewController.h>
+#import <Cephei/HBPreferences.h>
 
 @interface RLCModuleViewController : CCUIMenuModuleViewController
+
+@property (nonatomic, retain) HBPreferences *preferences;
 
 -(void)buttonTapped:(id)arg1 forEvent:(id)arg2 ;
 
--- RLCModuleViewController.m
+++ RLCModuleViewController.m
@@ -1,55 +1,65 @@
 #import "RLCModuleViewController.h"
-#import <Cephei/HBPreferences.h>
 
 @implementation RLCModuleViewController
+
+-(id)init {
+    self = [super init];
+    
+    self.preferences = [[HBPreferences alloc] initWithIdentifier:@"me.nepeta.relocate"];
+    [self.preferences registerPreferenceChangeBlock:^() {
+        if ([self isExpanded]) {
+            [self setSelected:NO];
+        } else {
+            [self setSelected:[([self.preferences objectForKey:@"Enabled"] ?: @(true)) boolValue]];
+        }
+
+        [self removeAllActions];
+
+        id obj = [self.preferences objectForKey:@"Favorites"];
+        if (obj && [obj isKindOfClass:[NSArray class]]) {
+            NSArray *favorites = obj;
+            __weak id weakSelf = self;
+            __weak id weakPrefs = self.preferences;
+            for (NSDictionary *favorite in favorites) {
+                [self addActionWithTitle:favorite[@"Name"] glyph:nil handler:^() {
+                    [weakSelf setSelected:YES];
+                    [weakPrefs setBool:YES forKey:@"Enabled"];
+
+                    NSMutableDictionary* globalLocation = [([weakPrefs objectForKey:@"GlobalLocation"] ?: @{}) mutableCopy];
+                    globalLocation[@"Coordinate"] = @{
+                        @"Latitude": favorite[@"Latitude"] ?: @(0),
+                        @"Longitude": favorite[@"Longitude"] ?: @(0)
+                    };
+                    [weakPrefs setObject:globalLocation forKey:@"GlobalLocation"];
+                    CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), (CFStringRef)@"me.nepeta.relocate/ReloadPrefs", nil, nil, true);
+                }];
+            }
+        }
+    }];
+
+    return self;
+}
 
 -(void)viewDidLayoutSubviews {
     if ([self isExpanded]) {
         [self setSelected:NO];
     } else {
-        HBPreferences *preferences = [[HBPreferences alloc] initWithIdentifier:@"me.nepeta.relocate"];
-        [self setSelected:[([preferences objectForKey:@"Enabled"] ?: @(true)) boolValue]];
+        [self setSelected:[([self.preferences objectForKey:@"Enabled"] ?: @(true)) boolValue]];
     }
-    
+
     [super viewDidLayoutSubviews];
 }
 
 
 -(void)viewWillAppear:(BOOL)animated {
     [super viewWillAppear:animated];
-
-    HBPreferences *preferences = [[HBPreferences alloc] initWithIdentifier:@"me.nepeta.relocate"];
-    [self setSelected:[([preferences objectForKey:@"Enabled"] ?: @(true)) boolValue]];
-    
-    [self removeAllActions];
-
-    id obj = [preferences objectForKey:@"Favorites"];
-    if (obj && [obj isKindOfClass:[NSArray class]]) {
-        NSArray *favorites = obj;
-        __weak id weakSelf = self;
-        for (NSDictionary *favorite in favorites) {
-            [self addActionWithTitle:favorite[@"Name"] glyph:nil handler:^() {
-                [weakSelf setSelected:YES];
-                [preferences setBool:YES forKey:@"Enabled"];
-
-                NSMutableDictionary* globalLocation = [([preferences objectForKey:@"GlobalLocation"] ?: @{}) mutableCopy];
-                globalLocation[@"Coordinate"] = @{
-                    @"Latitude": favorite[@"Latitude"] ?: @(0),
-                    @"Longitude": favorite[@"Longitude"] ?: @(0)
-                };
-                [preferences setObject:globalLocation forKey:@"GlobalLocation"];
-                CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), (CFStringRef)@"me.nepeta.relocate/ReloadPrefs", nil, nil, true);
-            }];
-        }
-    }
+    [self setSelected:[([self.preferences objectForKey:@"Enabled"] ?: @(true)) boolValue]];
 }
 
 -(void)buttonTapped:(id)arg1 forEvent:(id)arg2 {
     BOOL newState = ![self isSelected];
     [self setSelected:newState];
-    
-    HBPreferences *preferences = [[HBPreferences alloc] initWithIdentifier:@"me.nepeta.relocate"];
-    [preferences setBool:newState forKey:@"Enabled"];
+    [self.preferences setBool:newState forKey:@"Enabled"];
     CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), (CFStringRef)@"me.nepeta.relocate/ReloadPrefs", nil, nil, true);
 }
 
